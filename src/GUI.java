57e770535531f6640addb3924bb36b82641f03b1
ToddWorkLaptop <todd_garden@nal.com>
2020-07-23 12:20:01 -0500

    Added GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
+
+
 
     public static void createWindow(){
 
         JFrame frame = new JFrame("Mouse Clicker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel label = new JLabel("Time Interval");
+        JLabel label = new JLabel("Time Interval(1sec ~ 100sec): ");
+        JTextField textField = new JTextField(5);
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int var = Integer.parseInt(textField.getText());
+            }
+        });
+        System.out.println();
         label.setPreferredSize(new Dimension(300,100));
-        frame.getContentPane().add(label,BorderLayout.CENTER);
+        frame.getContentPane().add(label,BorderLayout.WEST);
+        frame.getContentPane().add(textField,BorderLayout.CENTER);
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
